73c73ecd13fbca3c8c9c2157799cdafa7fd7081c
Letur Berakler <LeturCZ@seznam.cz>
2023-11-18 16:26:53 +0100

    Login manager prereqs

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     from app.auth.routes import auth
     app.register_blueprint(auth)
