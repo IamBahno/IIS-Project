ac434fef73e48d14fa5ae6924686e636b6157965
IamBahno <obahounek@email.cz>
2023-11-18 14:22:25 +0100

    layout.html

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///itu_data.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///iis_data.db'
 
 class TestConfig:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
