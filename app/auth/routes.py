65fbab2a09452e3def6d0d3754fa53e04f7b6cab
IamBahno <obahounek@email.cz>
2023-11-23 17:35:31 +0100

    kpi display

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -6,7 +6,18 @@ from datetime import datetime
 
 auth = Blueprint('auth', __name__)
 
-
+#TODO pridat kpi do systemu, vsechny kpi projit, pokud jes aspon jedno "KO" hodit tam ko, nebo tak neco
+#TODO kpi delete
+#TODO nekam vypisovat veci jako kpi jmneo popis etc.
+#TODO u vytvareni kpi, udelat nejak ze musi byt nastavenej aspon jedna limit, kontrolovat ze jedna mensi hodnota je oprvdu mensi, regexi na zadavani cisel
+#TODO delete device v system deatilu nebo device detailu
+#TODO oddelat ze v register formulari si vybiras role, (mozna pridelaat ze admin muze davat ostatnim adim role)
+#TODO udelat edit ke vsemu (edit jmena osoby,kpi hodnoty, jmena device etc.....) bud predelat create page aby meli parameter create/edit 
+#                       a pak to tam dost prepsat nebo zkopirovat veci z create a predelat to na edit
+#TODO vypisovat vsude nejakej rozumnej header (treaba kdyz vytvaris device aby byl tam byl vypsanej system nebo tak neco )
+#TODO zajisti koretkni vstupy
+#TODO zajistit aby pri zadani neplatny hodnoty jako unikatni jmeno atd. nespadl program
+#TODO oznacit poviny pole, aby program nepadal pri nezadani tech nepoviny atd...
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
@@ -65,7 +76,6 @@ def register():
 def home():
     return redirect(url_for('auth.systems'))
 
-# TODO pridat kpi
 @auth.route("/systems",methods=['GET', 'POST'])
 def systems():
     if request.method == 'POST':
@@ -107,7 +117,6 @@ def systems():
     return render_template('systems.html',systems=systems)
 
 
-#TODO delete device
 @auth.route("/systems/<system_id>",methods=['GET', 'POST'])
 def system_detail(system_id):
     if request.method == "GET" and "device-detail" in request.values:
@@ -137,7 +146,11 @@ def system_detail(system_id):
         values = [Value.query.filter_by(parameter=parameter.id,device=device.id).order_by(Value.timestamp.desc()).first() for parameter in parameters]
         values_of_devices.append(values)
   
-    return render_template('system_detail.html',system=system,devices=devices,user=current_user,zip = zip,parameters = parameters_of_devices,values=values_of_devices)
+        #list of kpis for each parameter
+    kpis_of_devices = [[Kpi.query.filter_by(parameter_id=parameter.id,system=system_id).all() for parameter in parameters] for parameters in parameters_of_devices]
+    kpis_states = [get_kpi_states(values,kpis) for values,kpis in zip(values_of_devices,kpis_of_devices)]
+    return render_template('system_detail.html',system=system,devices=devices,user=current_user,zip = zip,parameters = parameters_of_devices,
+                           values=values_of_devices,kpis_of_devices=kpis_of_devices,kpis_states_of_devices=kpis_states)
 
 
 @auth.route("/systems/<system_id>/<device_id>",methods=['GET', 'POST'])
@@ -153,10 +166,16 @@ def device_detail(system_id, device_id):
         db.session.commit()
     device = Device.query.filter_by(id=device_id).first()
     device_type = DeviceType.query.filter_by(id=device.device_type_id).first()
+    #list of parameters
     parameters = device_type.parameters
+    #list of values
     values = [Value.query.filter_by(parameter=parameter.id,device=device.id).order_by(Value.timestamp.desc()).first() for parameter in parameters]
-
-    return render_template('device_detail.html', device_id=int(device_id), system_id=int(system_id),user=current_user,values=values,parameters=parameters,default_datetime=datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),zip=zip)
+    #list of kpis for each parameter
+    kpis = [Kpi.query.filter_by(parameter_id=parameter.id,system=system_id).all() for parameter in parameters]
+    #list of kpi states for parameters
+    kpi_states = get_kpi_states(values,kpis)
+    return render_template('device_detail.html', device_id=int(device_id), system_id=int(system_id),user=current_user,values=values,
+                           parameters=parameters,kpis=kpis,kpi_parameters_states=kpi_states,default_datetime=datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),zip=zip)
     
 @auth.route("/systems/<system_id>/create",methods=['GET', 'POST'])
 def device_create(system_id):
@@ -197,11 +216,44 @@ def system_create():
 
 @auth.route("/systems/<system_id>/kpi/create",methods=['GET','POST'])
 def kpi_create(system_id):
-    if "add-kpi" in request.values:
+    if "add-kpi-form" in request.values:
         parameters = parameters_of_system(system_id)
         return render_template('kpi_create.html',parameters=parameters)
-        
+    elif "create-kpi" in request.values:
+        lower_limit = float(request.values["lower_limit"]) if request.values["lower_limit"] != "" else None
+        upper_limit = float(request.values["upper_limit"]) if request.values["upper_limit"] != "" else None
+        kpi = Kpi(name=request.values["kpi_name"],description=request.values["kpi_description"],system=system_id,
+                  parameter_id=int(request.values['parameter']),lower_limit=lower_limit,upper_limit=upper_limit,creater=current_user.id)
+        db.session.add(kpi)
+        db.session.commit()
+        return redirect(f'/systems/{system_id}')
+
     return "<p>nevadi</p>"
+
+def get_kpi_states(values,kpis_for_parameters):
+    kpis_states_for_parameters = []
+    for kpis,value in zip(kpis_for_parameters,values):
+        kpis_states = []
+        for kpi in kpis:
+            if value == None or value.value == None:
+                kpis_states.append("KO")
+            elif kpi.lower_limit == None:
+                if value.value <= kpi.upper_limit:
+                    kpis_states.append("OK")
+                else:
+                    kpis_states.append("KO")
+            elif kpi.upper_limit == None:
+                if value.value >= kpi.lower_limit:
+                    kpis_states.append("OK")
+                else:
+                    kpis_states.append("KO")
+            else:
+                if value.value <= kpi.upper_limit and value.value >= kpi.lower_limit:
+                    kpis_states.append("OK")
+                else:
+                    kpis_states.append("KO")
+        kpis_states_for_parameters.append(kpis_states)
+    return kpis_states_for_parameters
 
 # @auth.route("/test",methods=['GET', 'POST'])
 # @login_required
