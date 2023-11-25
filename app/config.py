fa9dc5df107d77182e27067be96be7ee863fed87
IamBahno <obahounek@email.cz>
2023-11-25 20:45:25 +0100

    new db connection string

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///iis_data.db'
-    SQLALCHEMY_DATABASE_URI = "postgresql://iis_db_9hn8_user:gP6anVVqmvsth6ulLcGkiNWlzBhXp4zH@dpg-clf40gmf27hc73bifplg-a.frankfurt-postgres.render.com/iis_db_9hn8"
+    SQLALCHEMY_DATABASE_URI = "postgresql://iis_db_tjj3_user:VfJaoEv80IyqQ0mpxonXxMk0iK7Cgbvp@dpg-clh4rvmg1b2c73acl9f0-a.frankfurt-postgres.render.com/iis_db_tjj3"
 
 
 class TestConfig:
