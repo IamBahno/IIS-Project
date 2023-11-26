78dab7f1f668a74e32fb2d1fa135a5caa22439a1
IamBahno <obahounek@email.cz>
2023-11-27 00:42:53 +0100

    session timer

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ app = create_app()
 @app.before_request
 def my_before_request():
     session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=3)
+    app.permanent_session_lifetime = timedelta(minutes=30)
 if __name__ == '__main__':
     app.run(debug=True)
