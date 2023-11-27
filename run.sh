25402fbd99a8d59a83511451b3e57a008d227fb7
IamBahno <obahounek@email.cz>
2023-11-27 15:26:22 +0100

    new db uri

--- run.sh
+++ run.sh
@@ -6,9 +6,7 @@ else
     exit 1
 fi
 
-# # Run your Flask application
-# export FLASK_APP=app.py  # Replace with your Flask app entry point
-# export FLASK_ENV=development  # Set to 'production' for production
+
 
 venv/bin/python run.py
 
